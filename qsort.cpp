70afea4307040e999a57b3e4eba8d388cefdfd86
Alex Wang <ice_b0und@hotmail.com>
2020-07-18 00:05:20 +0800

    Add libstdc++ std::sort optimization

--- qsort.cpp
+++ qsort.cpp
@@ -5,9 +5,8 @@
 #include <vector>
 #include <chrono>
 
-// sort [start, end)
 template <typename T>
-void quicksort(std::vector<T> &arr, size_t start, size_t end) {
+void quicksort_simple(std::vector<T> &arr, size_t start, size_t end) {
   // len <= 1
   if (start + 1 >= end) {
     return;
@@ -42,8 +41,85 @@ void quicksort(std::vector<T> &arr, size_t start, size_t end) {
 }
 
 template <typename T>
+size_t median(std::vector<T> &arr, size_t a, size_t b, size_t c) {
+  if (arr[a] > arr[b] && arr[a] > arr[c]) {
+    if (arr[b] < arr[c]) {
+      return c;
+    } else {
+      return b;
+    }
+  } else if (arr[b] > arr[c] && arr[b] > arr[a]) {
+    if (arr[a] < arr[c]) {
+      return c;
+    } else {
+      return a;
+    }
+  } else {
+    if (arr[a] < arr[b]) {
+      return b;
+    } else {
+      return a;
+    }
+  }
+}
+
+// sort [start, end)
+template <typename T>
+void quicksort_loop(std::vector<T> &arr, size_t start, size_t end) {
+  while (true) {
+    // len <= 1
+    if (start + 1 >= end) {
+      return;
+    }
+
+    // len >= 2
+
+    // choose a median number as pivot to prevent worst case
+    auto mid = median(arr, start, (start+end)/2, end-1);
+    std::swap(arr[start], arr[mid]);
+
+    auto pivot = arr[start];
+    // [l, r) >= 1
+    size_t l = start+1, r = end;
+
+    while (l+1 < r) {
+      while (l+1 < r && arr[l] <= pivot) {
+        l++;
+      }
+      while (l+1 < r && arr[r-1] >= pivot) {
+        r--;
+      }
+      std::swap(arr[l], arr[r-1]);
+    }
+
+    size_t pivot_index = 0;
+    if (arr[l] <= pivot) {
+      std::swap(arr[start], arr[l]);
+      pivot_index = l;
+    } else {
+      // if l-1 == start, this is the worst case: pivot_index == start
+      std::swap(arr[start], arr[l-1]);
+      pivot_index = l-1;
+    }
+
+    // always recur the shorter first
+    if (pivot_index - start < end - (pivot_index+1)) {
+      quicksort_loop(arr, start, pivot_index);
+
+//      quicksort(arr, pivot_index+1, end);
+      start = pivot_index+1;
+    } else {
+      quicksort_loop(arr, pivot_index+1, end);
+
+//      quicksort(arr, start, pivot_index);
+      end = pivot_index;
+    }
+  }
+}
+
+template <typename T>
 void quicksort(std::vector<T> &arr) {
-  quicksort(arr, 0, arr.size());
+  quicksort_loop(arr, 0, arr.size());
 }
 
 int main() {
