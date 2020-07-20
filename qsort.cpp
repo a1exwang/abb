55c7af01b4082d56f960aa15bcb2547fe25e66ea
Alex Wang <ice_b0und@hotmail.com>
2020-07-20 21:40:18 +0800

    Add iterative qsort

--- qsort.cpp
+++ qsort.cpp
@@ -4,14 +4,11 @@
 #include <random>
 #include <vector>
 #include <chrono>
+#include <stack>
+#include <tuple>
 
 template <typename T>
-void quicksort_simple(std::vector<T> &arr, size_t start, size_t end) {
-  // len <= 1
-  if (start + 1 >= end) {
-    return;
-  }
-
+int partition(std::vector<T> &arr, int start, int end) {
   // len >= 2
   auto pivot = arr[start];
   // [l, r) >= 1
@@ -35,9 +32,42 @@ void quicksort_simple(std::vector<T> &arr, size_t start, size_t end) {
     std::swap(arr[start], arr[l-1]);
     pivot_index = l-1;
   }
+  return pivot_index;
+}
 
-  quicksort(arr, start, pivot_index);
-  quicksort(arr, pivot_index+1, end);
+template <typename T>
+void quicksort_iterative(std::vector<T> &arr) {
+  std::vector<std::tuple<int, int>> stack;
+  if (arr.size() != 0) {
+    stack.reserve(log2(arr.size()));
+  }
+  stack.push_back(std::make_tuple(0, arr.size()));
+  while (!stack.empty()) {
+    auto [start, end] = stack.back();
+    stack.pop_back();
+    if (start+1 < end) {
+      auto pivot_index = partition(arr, start, end);
+      if (end - (pivot_index+1) > 1) {
+        stack.push_back(std::make_tuple(pivot_index+1, end));
+      }
+      if (pivot_index-start > 1) {
+        stack.push_back(std::make_tuple(start, pivot_index));
+      }
+    }
+  }
+}
+
+
+template <typename T>
+void quicksort_simple(std::vector<T> &arr, size_t start, size_t end) {
+  // len <= 1
+  if (start + 1 >= end) {
+    return;
+  }
+  auto pivot_index = partition(arr, start, end);
+
+  quicksort_simple(arr, start, pivot_index);
+  quicksort_simple(arr, pivot_index+1, end);
 }
 
 template <typename T>
@@ -130,18 +160,33 @@ int main() {
   std::generate(a.begin(), a.end(), [&]() { return dist(rng); });
   auto a1 = a;
   auto a2 = a;
+  auto a3 = a;
+  auto a4 = a;
   auto t0 = std::chrono::high_resolution_clock::now();
   std::sort(a1.begin(), a1.end());
   auto t1 = std::chrono::high_resolution_clock::now();
   quicksort(a2);
   auto t2 = std::chrono::high_resolution_clock::now();
+  quicksort_iterative(a3);
+  auto t3 = std::chrono::high_resolution_clock::now();
+  quicksort_simple(a4, 0, a4.size());
+  auto t4 = std::chrono::high_resolution_clock::now();
   for (int i = 0; i < a1.size(); i++) {
     if (a1[i] != a2[i]) {
       std::cerr << "result not matched " << std::endl;
       return 1;
     }
   }
+  for (int i = 0; i < a1.size(); i++) {
+    if (a1[i] != a3[i]) {
+      std::cerr << "iter result not matched " << std::endl;
+      return 1;
+    }
+  }
   std::cout << "all tests have passed" << std::endl;
-  std::cout << "std::sort: " << std::chrono::duration<double, std::milli>(t1-t0).count() << "ms, "
-            << "quicksort: " << std::chrono::duration<double, std::milli>(t2-t1).count() << std::endl;
+  std::cout << "std::sort: " << std::chrono::duration<double, std::milli>(t1-t0).count() << "ms" << std::endl
+            << "quicksort opt: " << std::chrono::duration<double, std::milli>(t2-t1).count() << "ms" << std::endl
+            << "quicksort iter: " << std::chrono::duration<double, std::milli>(t3-t2).count() << "ms" << std::endl
+            << "quicksort simple: " << std::chrono::duration<double, std::milli>(t4-t3).count() << "ms" << std::endl
+      ;
 }
